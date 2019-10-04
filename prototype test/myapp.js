2f947cbf3caad2c2b086f403fdfa2185a5221d25
ajfunelas <31107247+ajfunelas@users.noreply.github.com>
2019-10-04 11:29:31 +0800

    new player

--- prototype test/myapp.js
+++ prototype test/myapp.js
@@ -4,18 +4,26 @@ class player {
         this.energy = energy
     }
     eat(amount) {
-        console.log('${this.name} is eating.')
+        console.log(`${this.name} is eating.`)
         this.energy += amount
     }
     sleep(length) {
-        console.log('${this.name} is sleeping.')
+        console.log(`${this.name} is sleeping.`)
         this.energy += length
     }
     play(length) {
-        console.log('${this.name} is playing.')
+        console.log(`${this.name} is playing.`)
         this.energy += length
     }
 }
 
-const aj = new player('Abner', 100)
-const kim = new player('Kim', 200)
+function archer(name, energy, level){
+    player.call(this, name, energy)
+
+    this.level = level
+}
+
+const aj = new player('Abner', 100, 99)
+const kim = new player('Kim', 200, 98)
+
+const archer1 = new player ('archer', 100, 99)
